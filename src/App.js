32cc8e1af31762ac37b37ff570fba2a5718d0975
ajumape <ajuv05@gmail.com>
2020-07-27 17:20:34 -0400

    changed appliation information

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <!--// Edit <code>src/App.js</code> and save to reload 7. [this is the old line of message]//-->
-          I was changed on the feature branch.
+          I was changed on the feature branch....
         </p>
         <a
           className="App-link"
